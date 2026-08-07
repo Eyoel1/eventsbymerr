17c85a8617ef61485aa84cf3efee2dfe511427fb
Eyoel Goshu <eyumaneyu321@gmail.com>
2026-08-07 07:57:56 -0300

    Initial commit from Create Next App

---
+++ src/lib/db/db.ts
@@ -0,0 +1,38 @@
+import Dexie, { type Table } from 'dexie';
+import type {
+  Profile,
+  Exercise,
+  Program,
+  ProgramDay,
+  WorkoutSession,
+  SetLog,
+  BodyMetric,
+  Review,
+} from './schema';
+
+export class MuscleCoachDB extends Dexie {
+  profiles!: Table<Profile>;
+  exercises!: Table<Exercise>;
+  programs!: Table<Program>;
+  programDays!: Table<ProgramDay>;
+  workoutSessions!: Table<WorkoutSession>;
+  setLogs!: Table<SetLog>;
+  bodyMetrics!: Table<BodyMetric>;
+  reviews!: Table<Review>;
+
+  constructor() {
+    super('MuscleCoachDB');
+    this.version(1).stores({
+      profiles: '++id',
+      exercises: '++id, primaryMuscle, equipment, difficultyTier',
+      programs: '++id, status',
+      programDays: '++id, programId, order',
+      workoutSessions: '++id, programDayId, date, status',
+      setLogs: '++id, workoutSessionId, exerciseId, createdAt',
+      bodyMetrics: '++id, date',
+      reviews: '++id, type, periodStart',
+    });
+  }
+}
+
+export const db = new MuscleCoachDB();
